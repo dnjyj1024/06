1b712d562b90d7accadb890972236744db31ea3e
dnjyj1024 <dnjyj1024@naver.com>
2022-10-11 16:29:20 +0900

    01_1

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
+void print_star()
+{
 	int i;
+	for(i=0; i<10;i++)
+	printf("*");
+}
+int main(void) {
 	
-	for(i = 0; i<10; i++)
-	printf("*");
-	for(i = 0; i<10; i++)
-	printf("*");
-	for(i = 0; i<10; i++)
-	printf("*");
+	print_star();
+	print_star();
+	print_star();
 	return 0;
 }

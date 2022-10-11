a29279dfdb2a12e739b2bd05f3be23d62712a0fe
dnjyj1024 <dnjyj1024@naver.com>
2022-10-11 17:04:11 +0900

    04_1

--- main.c
+++ main.c
@@ -3,25 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a,int b)
+void square(int a)
 {
-	printf("%d\n",a+b);
+	a=a*a;
 }
-int square(int n)
-{
-	printf("%d\n",n*n);
-}
-int get_max(int x,int y)
-{
-	if (x>y)
-	printf("%d\n",x);
-	else
-	printf("%d\n",y);
-}
-int main(void) {
-	sumTwo(2,3);
-	square(2);
-	get_max(5,3);
-	
-	return 0;
+
+int main(){
+	int a = 2;
+	square(a);
+	printf("a=%i\n",a);
 }
